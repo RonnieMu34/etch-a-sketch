8a201ca8d581efd2cad906a60442a2a8806d23b9
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-02 13:33:48 -0400

    Add eventlistener mouseover event for square cells

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".grid-container");
+const gridCell = document.querySelector(".square");
 
 // for (let i = 0; i < 256; i++) {
 //     let square = document.createElement('div');
@@ -6,13 +7,17 @@ const container = document.querySelector(".grid-container");
 //     container.appendChild(square);
 // }
 
-for (let x = 0; x < 5; x++) {
+for (let x = 0; x < 10; x++) {
     let newRow = document.createElement('div');
-    newRow.classList = `grid-row ${x + 1}`;
+    newRow.classList = `grid-row`;
     container.appendChild(newRow);
-    for (let y = 0; y < 5; y++) {
+    for (let y = 0; y < 10; y++) {
         let square = document.createElement('div');
-        square.classList = `square ${y + 1}`;
+        square.classList = `square`;
         newRow.appendChild(square);
         }
 }
+
+gridCell.addEventListener("mouseover", () => {
+    gridCell.classList.add('hov-square');
+});
