7fbcfa0b59b5bb606a80edc9cb01ac1da7f33e26
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-08 11:50:01 -0400

    Create squares and add them to grid container

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let squares = 5;
 
 container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
 container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+
+for (let i = 0; i < (squares * squares); i++) {
+    let square = document.createElement('div');
+    square.classList.add("square");
+    container.appendChild(square);
+}
