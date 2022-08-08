7690df1208d9783e2e3c77651f3506faaefacff0
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-08 11:54:57 -0400

    Create function to activate CSS color change property

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".grid-container");
 
-let squares = 5;
+let squares = 10;
 
 container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
 container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
@@ -10,3 +10,11 @@ for (let i = 0; i < (squares * squares); i++) {
     square.classList.add("square");
     container.appendChild(square);
 }
+
+const gridCells = document.querySelectorAll(".square");
+
+gridCells.forEach(cell => {
+    cell.addEventListener("mouseover", () => {
+        cell.classList.add("hov-square");
+    });
+});
