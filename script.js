8ac6c8880646ef08d0fc6e731925e79687380fe5
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-08 14:03:34 -0400

    Create button to submit user form for grid size

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const container = document.querySelector(".grid-container");
+var btn = document.getElementById("submit");
 
-let squares = 10;
+btn.addEventListener("click", () => {
+    let squares = document.getElementById("number").value;
 
-container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
-container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
 
-for (let i = 0; i < (squares * squares); i++) {
-    let square = document.createElement('div');
-    square.classList.add("square");
-    container.appendChild(square);
-}
+    for (let i = 0; i < (squares * squares); i++) {
+        let square = document.createElement('div');
+        square.classList.add("square");
+        container.appendChild(square);
+    }
 
-const gridCells = document.querySelectorAll(".square");
+    const gridCells = document.querySelectorAll(".square");
 
-gridCells.forEach(cell => {
-    cell.addEventListener("mouseover", () => {
-        cell.classList.add("hov-square");
+    gridCells.forEach(cell => {
+        cell.addEventListener("mouseover", () => {
+            cell.classList.add("hov-square");
+        });
     });
+
 });
