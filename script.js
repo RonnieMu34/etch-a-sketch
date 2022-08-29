2fce7869513bcfaee3a7e8dabad6f0870de9801c
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-29 13:25:52 -0400

    Revert project to earlier version

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const container = document.querySelector(".grid-container");
-var slider = document.getElementById("myRange");
+var btn = document.getElementById("submit");
 var reset = document.getElementById("reset");
-const gridCells = document.querySelectorAll(".square");
 
-    slider.addEventListener("change", () => {
-        let squares = slider.value;
+btn.addEventListener("click", () => {
+    let squares = document.getElementById("number").value;
 
-        container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
-        container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
 
-        for (let i = 0; i < (squares * squares); i++) {
-            let square = document.createElement('div');
-            square.classList.add("square");
-            container.appendChild(square);
-        }
+    for (let i = 0; i < (squares * squares); i++) {
+        let square = document.createElement('div');
+        square.classList.add("square");
+        container.appendChild(square);
+    }
 
-        gridCells.forEach(cell => {
-            cell.addEventListener("mouseover", () => {
-                cell.classList.add("hov-square");
-            });
+    const gridCells = document.querySelectorAll(".square");
+
+    gridCells.forEach(cell => {
+        cell.addEventListener("mouseover", () => {
+            cell.classList.add("hov-square");
         });
+    });
+
+    reset.addEventListener("click", () => {
+        gridCells.forEach(cell => {
+            cell.classList.remove("hov-square");
+            container.removeChild(cell);
+        });
+    });
+
 });
-console.log(slider.value);
