f6f61f80c08de1596379a306457225dbde72e554
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-30 11:09:59 -0400

    Make the grid reset after the slider changes value

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const container = document.querySelector(".grid-container");
 var slider = document.getElementById("slider");
 var reset = document.getElementById("reset");
 
-slider.addEventListener("change", () => {
+slider.addEventListener("change", () => { // This is the slider event listener
     let squares = slider.value;
+
+    container.innerHTML = "";
 
     container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
@@ -16,13 +18,13 @@ slider.addEventListener("change", () => {
 
     const gridCells = document.querySelectorAll(".square");
 
-    gridCells.forEach(cell => {
+    gridCells.forEach(cell => { // This is how the new property is added to grey-out a square after a square is hovered over
         cell.addEventListener("mouseover", () => {
             cell.classList.add("hov-square");
         });
     });
 
-    reset.addEventListener("click", () => {
+    reset.addEventListener("click", () => { // This was the reset button I used previously, kept it here to let you see what I did to make it
         gridCells.forEach(cell => {
             cell.classList.remove("hov-square");
             container.removeChild(cell);
