421e7c1abe74d375765f309b15d6fe94a040f28e
RonnieMu34 <ronnie34mu@gmail.com>
2022-08-31 11:26:35 -0400

    Remove ClickAndHold class

--- script.js
+++ script.js
@@ -24,38 +24,3 @@ slider.addEventListener("change", () => { // This is the slider event listener
     });
 
 });
-
-class ClickAndHold {
-    /**
-     * 
-     * @param {EventTarget} target The HTML element to apply the event to 
-     * @param {Function} callback The function to run once the target is clicked and held 
-     */
-    constructor(target, callback) {
-        this.target = target;
-        this.callback = callback;
-        this.isHeld = false; // By default, user isn't holding down button
-        this.activeHoldTimeoutId = null;
-
-        ["mousedown", "touchstart"].forEach(type => {
-            this.target.addEventListener(type, this._onHoldStart.bind(this));
-        });
-
-        ["mouseup", "mouseleave", "mouseout", "touchend", "touchcancel"].forEach(type => {
-            this.target.addEventListener(type, this._onHoldEnd.bind(this));
-        });
-    }
-    _onHoldStart() {
-        this.isHeld = true;
-        this.activeHoldTimeoutId = setTimeout(() => {
-            if (this.isHeld) {
-                this.callback();
-            }
-        }, 1000);
-    }
-
-    _onHoldEnd() {
-        this.isHeld = false;
-        clearTimeout(this.activeHoldTimeoutId);
-    }
-}
